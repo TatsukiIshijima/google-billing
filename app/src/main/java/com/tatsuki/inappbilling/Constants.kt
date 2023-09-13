08dc4feeae3c1a92e91a429dc42bb7773f45613d
TatsukiIshijima <tatsuki445@gmail.com>
2023-09-13 12:06:22 +0900

    add

--- app/src/main/java/com/tatsuki/inappbilling/Constants.kt
+++ app/src/main/java/com/tatsuki/inappbilling/Constants.kt
@@ -8,6 +8,7 @@ object Constants {
   // ProductIds
   const val TEST_IN_APP_BILLING_SUBSCRIPTION_PLAN_1 = "test.inappbilling.subscription.plan1"
   const val TEST_IN_APP_BILLING_SUBSCRIPTION_PLAN_2 = "test.inappbilling.subscription.plan2"
+  const val TEST_IN_APP_BILLING_SUBSCRIPTION_PLAN_3 = "test.inappbilling.subscription.plan3"
 
   // PlanIds
   // Plan id same as plan id in this app.
@@ -16,6 +17,8 @@ object Constants {
   const val WEEKLY_PLAN = "weekly-plan"
   const val BASIC_PLAN = "basic-plan"
   const val PREMIUM_PLAN = "premium-plan"
+  const val ESSENTIAL_PLAN = "essential-plan"
+  const val EXTRA_PLAN = "extra-plan"
 
   // OfferIds
   // Offer id same as offer id in this app.
