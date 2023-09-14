d4516351fddb6303c718fbf22a538f3daedd936f
TatsukiIshijima <tatsuki445@gmail.com>
2023-09-14 10:06:48 +0900

    up

--- google/build.gradle.kts
+++ google/build.gradle.kts
@@ -44,7 +44,7 @@ publishing {
     register<MavenPublication>("release") {
       groupId = "com.github.TatsukiIshijima"
       artifactId = "google-billing"
-      version = "0.0.3"
+      version = "0.0.4"
 
       afterEvaluate {
         from(components["release"])
