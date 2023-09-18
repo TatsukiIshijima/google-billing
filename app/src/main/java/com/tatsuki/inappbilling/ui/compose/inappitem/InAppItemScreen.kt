58961cbca395c5dc65fa54c1debbf644d2cd7196
TatsukiIshijima <tatsuki445@gmail.com>
2023-09-18 10:46:39 +0900

    create tab screens

---
+++ app/src/main/java/com/tatsuki/inappbilling/ui/compose/inappitem/InAppItemScreen.kt
@@ -0,0 +1,11 @@
+package com.tatsuki.inappbilling.ui.compose.inappitem
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun InAppItemScreen(
+  modifier: Modifier = Modifier
+) {
+
+}
---
+++ app/src/main/java/com/tatsuki/inappbilling/ui/compose/subscription/SubscriptionScreen.kt
@@ -0,0 +1,11 @@
+package com.tatsuki.inappbilling.ui.compose.subscription
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SubscriptionScreen(
+  modifier: Modifier = Modifier
+) {
+
+}
