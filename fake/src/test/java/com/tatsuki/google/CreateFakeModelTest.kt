70237ca94c58cbfe66dcc2b77927be220cdf44c2
TatsukiIshijima <tatsuki445@gmail.com>
2023-10-01 17:25:42 +0900

    create onetime purchase offer details fake class

--- fake/src/test/java/com/tatsuki/google/CreateFakeModelTest.kt
+++ fake/src/test/java/com/tatsuki/google/CreateFakeModelTest.kt
@@ -1,5 +1,6 @@
 package com.tatsuki.google
 
+import com.tatsuki.google.billing.fake.model.FakeOneTimePurchaseOfferDetails
 import com.tatsuki.google.billing.fake.model.FakePricingPhase
 import com.tatsuki.google.billing.fake.model.FakePricingPhases
 import org.junit.Test
@@ -18,5 +19,12 @@ class CreateFakeModelTest {
     val fakePricingPhases = FakePricingPhases.create()
     val realPricingPhases = fakePricingPhases.toReal()
     assert(fakePricingPhases.list.size == realPricingPhases.pricingPhaseList.size)
+  }
+
+  @Test
+  fun create_fake_one_time_purchase_offer_details() {
+    val fakeOneTimePurchaseOfferDetails = FakeOneTimePurchaseOfferDetails.create()
+    val realOneTimePurchaseOfferDetails = fakeOneTimePurchaseOfferDetails.toReal()
+    assert(fakeOneTimePurchaseOfferDetails.formattedPrice == realOneTimePurchaseOfferDetails.formattedPrice)
   }
 }
