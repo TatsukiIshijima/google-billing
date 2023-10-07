1dca6a7d89ebec9ff668ca2ae27403f42eb76252
TatsukiIshijima <tatsuki445@gmail.com>
2023-10-07 13:43:36 +0900

    setup publish

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,7 +1,15 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
   id("com.android.library")
   id("org.jetbrains.kotlin.android")
+  id("maven-publish")
 }
+
+val libVersionsPropertiesFile = rootProject.file("libversions.properties")
+val libVersionsProperties = Properties()
+libVersionsProperties.load(FileInputStream(libVersionsPropertiesFile))
 
 android {
   namespace = "com.tatsuki.billing.core"
@@ -34,4 +42,19 @@ dependencies {
 
   api("com.android.billingclient:billing:6.0.1")
   api("com.android.billingclient:billing-ktx:6.0.1")
+}
+
+publishing {
+  publications {
+    register<MavenPublication>("release") {
+//      groupId = "com.github.TatsukiIshijima"
+//      artifactId = project.name
+//      artifactId = rootProject.name
+      version = libVersionsProperties["VERSION_NAME"] as String
+
+      afterEvaluate {
+        from(components["release"])
+      }
+    }
+  }
 }
--- fake/build.gradle.kts
+++ fake/build.gradle.kts
@@ -1,7 +1,15 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
   id("com.android.library")
   id("org.jetbrains.kotlin.android")
+  id("maven-publish")
 }
+
+val libVersionsPropertiesFile = rootProject.file("libversions.properties")
+val libVersionsProperties = Properties()
+libVersionsProperties.load(FileInputStream(libVersionsPropertiesFile))
 
 android {
   namespace = "com.tatsuki.billing.fake"
@@ -36,10 +44,25 @@ android {
 }
 
 dependencies {
-  // TODO: get from jitpack
+//  implementation("com.github.TatsukiIshijima:google-billing:core:change-multi-module-SNAPSHOT")
   implementation(project(":core"))
   implementation("com.google.code.gson:gson:2.10.1")
   testImplementation("org.json:json:20230618")
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
+}
+
+publishing {
+  publications {
+    register<MavenPublication>("release") {
+//      groupId = "com.github.TatsukiIshijima"
+//      artifactId = project.name
+//      artifactId = rootProject.name
+      version = libVersionsProperties["VERSION_NAME"] as String
+
+      afterEvaluate {
+        from(components["release"])
+      }
+    }
+  }
 }
