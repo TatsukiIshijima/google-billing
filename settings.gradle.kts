d16c9c8cea701436495e1e233f7790f5fa0eb3fc
TatsukiIshijima <tatsuki445@gmail.com>
2023-10-14 20:55:02 +0900

    rename

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,11 +10,11 @@ dependencyResolutionManagement {
   repositories {
     google()
     mavenCentral()
-    maven { url 'https://jitpack.io' }
+    maven(url = "https://jitpack.io")
   }
 }
 rootProject.name = "google-billing"
-include ':app'
-include ':feature'
-include ':fake'
-include ':core'
+include(":app")
+include(":feature")
+include(":fake")
+include(":core")
